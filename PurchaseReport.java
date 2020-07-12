5ee0d5c6bcfd41569ecabcc2b63adba4559a6bd2
sitjess <68169840+sitjess@users.noreply.github.com>
2020-07-11 21:01:24 -0600

    Add files via upload

---
+++ PurchaseReport.java
@@ -0,0 +1,19 @@
+//Jessica Sit
+package PurchaseReport;
+
+public class PurchaseReport 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("=========================");
+		System.out.println("Purchase Report");
+		System.out.println("=========================");
+		System.out.printf("Bob\t\t  $%.2f\n",21.99);
+		System.out.printf("Pants\t\t  $%.2f\n",49.97);
+		System.out.printf("Shoes\t\t  $%.2f\n",89.50);
+		System.out.printf("Belt\t\t  $%.2f\n",19.99);
+		System.out.printf("Coat\t\t $%.2f\n",129.95);
+		System.out.println("========================");
+		System.out.printf("Total\t\t $%.2f\n",311.40);
+	}
+}
